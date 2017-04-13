746d4565254f3ad0f7323348ed69113160126ac9
KamranG12 <Kamran.J@aplusa-security.com>
2017-04-13 18:55:46 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
           x=$(this).text();
           leftvalue = $(".test").eq(x-1).css('left');
           console.log(leftvalue)
-          if(leftvalue=='1500px'){
+          if(leftvalue=='2000px'){
           $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "35000");
           $(".test").eq(x-1).prevAll().css('z-index','10').animate({left: '0'}, "35000");
           }else if(x!=1){
-           $(".test").eq(x).css('z-index','10').animate({left: '1500'}, "35000");
-           $(".test").eq(x).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
+           $(".test").eq(x).css('z-index','10').animate({left: '2000'}, "35000");
+           $(".test").eq(x).nextAll().css('z-index','10').animate({left: '2000'}, "35000");
           }else{
-              $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
+              $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '2000'}, "35000");
           }
 }); 
 });
