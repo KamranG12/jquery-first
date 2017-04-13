89d12aab8661f32f63cb5e75445ed7003c5dec9c
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-04-13 17:04:19 +0400

    end_update1

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
           $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "35000");
           $(".test").eq(x-1).prevAll().css('z-index','10').animate({left: '0'}, "35000");
           }else if(x!=1){
-           $(".test").eq(x).css('z-index','10').animate({left: '1500'}, "25000");
+           $(".test").eq(x).css('z-index','10').animate({left: '1500'}, "35000");
            $(".test").eq(x).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
           }else{
               $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
