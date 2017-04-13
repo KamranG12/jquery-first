66c707a2b142ab80789a72d8cff5761e08e1d22b
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-04-13 11:39:09 +0400

    second

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
      var x=0;
+     var images=["image/img1.jpg","image/img2.jpg","image/img3.jpg","image/img4.jpg"];
       $(".bir").click(function(){
           x=$(this).text();
           console.log(x)
-    $(".ekran").eq(x).css('z-index','10').animate({left: '0'},1000);
-    console.log($(".ekran"))
+          $('.img1 img').attr("src", images[x-1]);
+          $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "4000");
 }); 
 });
