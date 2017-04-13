5a0d054d45ef180124e5f516a1dd55ded00e88c0
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-04-13 17:03:29 +0400

    end_update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
      var x=0;
-     var images=["image/img1.jpg","image/img2.jpg","image/img3.jpg","image/img4.jpg"];
       $(".bir").click(function(){
           x=$(this).text();
           leftvalue = $(".test").eq(x-1).css('left');
           console.log(leftvalue)
           if(leftvalue=='1500px'){
-          $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "4000");
-          $(".test").eq(x-1).prevAll().css('z-index','10').animate({left: '0'}, "4000");
-          $('.img1 img').attr("src", images[x-1]);
+          $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "35000");
+          $(".test").eq(x-1).prevAll().css('z-index','10').animate({left: '0'}, "35000");
           }else if(x!=1){
-           $(".test").eq(x).css('z-index','10').animate({left: '1500'}, "4000");
-           $(".test").eq(x).nextAll().css('z-index','10').animate({left: '1500'}, "4000");
-           $('.img1 img').attr("src", images[x-2]);
+           $(".test").eq(x).css('z-index','10').animate({left: '1500'}, "25000");
+           $(".test").eq(x).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
           }else{
-              $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "4000");
-              $('.img1 img').attr("src", images[x-1]);
+              $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "35000");
           }
 }); 
 });
