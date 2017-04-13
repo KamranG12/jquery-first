3bc0afcdb037846e59c9f19879881d1590233af5
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-04-13 15:51:27 +0400

    last

--- js/script.js
+++ js/script.js
@@ -7,10 +7,15 @@ $(document).ready(function() {
           console.log(leftvalue)
           if(leftvalue=='1500px'){
           $(".test").eq(x-1).css('z-index','10').animate({left: '0'}, "4000");
+          $(".test").eq(x-1).prevAll().css('z-index','10').animate({left: '0'}, "4000");
           $('.img1 img').attr("src", images[x-1]);
           }else if(x!=1){
            $(".test").eq(x-1).css('z-index','10').animate({left: '1500'}, "4000");
+           $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "4000");
            $('.img1 img').attr("src", images[x-2]);
+          }else{
+              $(".test").eq(x-1).nextAll().css('z-index','10').animate({left: '1500'}, "4000");
+              $('.img1 img').attr("src", images[x-1]);
           }
 }); 
 });
